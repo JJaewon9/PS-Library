26b58c1a975dc31ee1b23adfa89235fa204292a9
Jaewon Koo <62303011+JJaewon9@users.noreply.github.com>
2025-12-06 11:20:39 +0900

    Add template for competitive programming in C++

---
+++ misc/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define all(x) x.begin(),x.end()
+const int INF=1e9;
+const ll LINF=1e18;
+
+void TESTCASE() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int T=1; //cin>>T;
+    for (int tc=1;tc<=T;tc++) {
+        TESTCASE();
+    }
+    return 0;
+}
