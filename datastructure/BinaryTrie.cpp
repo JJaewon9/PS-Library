0c9d59d54b7be21e2e1b1fc604972141047e1b18
Jaewon Koo <62303011+JJaewon9@users.noreply.github.com>
2025-12-06 13:22:56 +0900

    Implement Binary Trie data structure

---
+++ datastructure/BinaryTrie.cpp
@@ -0,0 +1,30 @@
+struct BTRIE{
+    vector<int> ch[2],sz;
+    BTRIE(){
+        ch[0].push_back(-1);
+        ch[1].push_back(-1);
+        sz.push_back(0);
+    }
+    void add(int x){
+        int curr=0;
+        for(int i=30;i>=0;i--){
+            int b=(x>>i)&1;
+            if(ch[b][curr]==-1){
+                ch[0].push_back(-1);
+                ch[1].push_back(-1);
+                sz.push_back(0);
+                ch[b][curr]=(int)sz.size()-1;
+            }
+            curr=ch[b][curr];
+            sz[curr]++;
+        }
+    }
+    int query(int x){ // xor min
+        int curr=0;
+        for(int i=30;i>=0;i--){
+            int b=(x>>i)&1;
+            if(ch[b][curr]!=-1) curr=ch[b][curr];
+            else curr=ch[b^1][curr];
+        }
+    }
+};
