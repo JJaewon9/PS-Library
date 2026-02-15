0621dc148afe68ade77ad45dc4e6615b1652b184
Jaewon Koo <62303011+JJaewon9@users.noreply.github.com>
2026-02-15 13:52:47 +0900

    Modify LINF and add debug utilities
    Updated the LINF constant and added debug utilities for better logging and testing.

--- misc/Template.cpp
+++ misc/Template.cpp
@@ -3,7 +3,43 @@ using namespace std;
 using ll=long long;
 #define all(x) x.begin(),x.end()
 const int INF=1e9;
-const ll LINF=1e18;
+const ll LINF=4e18;
+
+// DEBUG tempalte
+string to_string(const string& s) { return '"'+s+'"'; }
+string to_string(bool b) { return b ? "true" : "false"; }
+template <typename A,typename B> string to_string(pair<A,B> p) { return "("+to_string(p.first)+", "+to_string(p.second)+")"; }
+template <typename A,typename B,typename C> string to_string(tuple<A,B,C> p) { return "("+to_string(get<0>(p))+", "+to_string(get<1>(p))+", "+to_string(get<2>(p))+")"; }
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) res += ", ";
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+void DEBUG_OUT() { cerr << endl; }
+template <typename Head,typename... Tail> void DEBUG_OUT(Head H, Tail... T) { cerr << " " << to_string(H); DEBUG_OUT(T...); }
+#ifdef ONLINE_JUDGE
+#define DEBUG(...)
+#else
+#define DEBUG(...) cerr << "[" << #__VA_ARGS__ << "]:", DEBUG_OUT(__VA_ARGS__)
+#endif
+
+// multidimentional vector
+template <class T>
+auto makeVector(size_t n, const T& val) {
+    return vector<T>(n, val);
+}
+template <class T, class... Sizes>
+auto makeVector(size_t n, size_t m, Sizes... sizes, const T& val) {
+    auto inner = makeVector<T>(m, sizes..., val);
+    return vector<decltype(inner)>(n, inner);
+}
 
 void TESTCASE() {
     
