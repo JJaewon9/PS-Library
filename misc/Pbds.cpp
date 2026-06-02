5f26800ebd4f3ecd40a320b48bb2e41a5bffe86f
Jaewon Koo <62303011+JJaewon9@users.noreply.github.com>
2026-06-02 12:59:18 +0900

    Update Pbds.cpp

--- misc/Pbds.cpp
+++ misc/Pbds.cpp
@@ -3,3 +3,8 @@
 using namespace __gnu_pbds;
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
 // order_of_key, find_by_order
+void m_erase(ordered_set &OS, int val){
+    int index = OS.order_of_key(val);
+    ordered_set::iterator it = OS.find_by_order(index);
+    OS.erase(it);
+}
